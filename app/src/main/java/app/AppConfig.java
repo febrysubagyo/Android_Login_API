5e9648f601d3f9f9b85267cd3a7a33368d9c314c
Febry Muchammad <mc.ebryl@gmail.com>
2016-11-23 10:35:17 +0700

    Login Register

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by Febry Muchammad on 23/11/2016.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://10.15.22.180/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+}
