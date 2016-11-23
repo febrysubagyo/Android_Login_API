c1e6f3fa36a00f43631084add4351324ca4d3efd
Febry Muchammad <mc.ebryl@gmail.com>
2016-11-23 13:42:07 +0700

    Login Register 3

--- app/src/main/java/app/AppConfig.java
+++ app/src/main/java/app/AppConfig.java
@@ -9,5 +9,5 @@ public class AppConfig {
     public static String URL_LOGIN = "http://10.15.22.180/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+    public static String URL_REGISTER = "http://10.15.22.180/android_login_api/register.php";
 }
